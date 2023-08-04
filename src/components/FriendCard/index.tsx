a1c05962878eaa78bd217ea4216dd94f09ad0469
nassorc <macrossan17@gmail.com>
2023-08-04 02:01:41 -0700

    Added ProfileImage component to the friend list components

--- src/components/FriendCard/index.tsx
+++ src/components/FriendCard/index.tsx
@@ -1,16 +1,19 @@
 import { cn } from "@/lib/utils"
 import { Button } from "../ui/button"
+import UserProfilePopover from "../UserProfilePopover"
+import ProfileImage from "../ProfileImage"
 interface PropsType {
   id: string
   username: string
   online: boolean
   playing: boolean
+  profileImage: string
   disabled?: boolean
   inviteFriend: (id: string) => void
 }
 
 export function FriendCard(props: PropsType) {
-  const { id, username, online, playing, inviteFriend, disabled } = props;
+  const { id, username, online, playing, inviteFriend, profileImage, disabled } = props;
   
   const handleInviteFriend = () => {
     console.log("inviting")
@@ -20,7 +23,12 @@ export function FriendCard(props: PropsType) {
   return (
     <div key={id} className="mx-auto py-1 px-3 flex border-t-2 last:border-b-2 justify-between border-[#ebebeb] text-[#0b132b]">
       <div className="flex items-center gap-x-2">
-        <p className="font-bold">{username}</p>
+        <UserProfilePopover userId={id}>
+          <div className="flex items-center space-x-2">
+            <ProfileImage profileImage={profileImage} size="sm"/>
+            <p className="font-bold">{username}</p>
+          </div>
+        </UserProfilePopover>
         <div className="min-w-content min-h-content">
           <div className={`w-3 h-3 inline-block rounded-full ${(online) ? "bg-green-400" : "bg-red-400"}`}>
           </div>
