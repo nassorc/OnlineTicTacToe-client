5a8a56a0955056de3e14d203c30a899ee3575337
nassorc <macrossan17@gmail.com>
2023-08-06 16:28:41 -0700

    Added delete account alert dialog

--- src/pages/Profile/ProfilePage.tsx
+++ src/pages/Profile/ProfilePage.tsx
@@ -10,10 +10,23 @@ import {
   DropdownMenuTrigger,
 
 } from "@/components/ui/dropdown-menu";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
 import firebaseApp, { storage, ref, uploadBytes, getDownloadURL } from "../../config/firebase";
 import UserContext from "@/context/user";
+import { logoutUser, AUTH_ACTIONS, useAuth } from "@/context/auth";
 
 export default function ProfilePage() {
+  const [authState, authDispatch] = useAuth();
   const { user, setUser } = useContext(UserContext);
   const [isUploading, setIsUploading] = useState(false);
   const onUploadImage = async (e) => {
@@ -41,8 +54,12 @@ export default function ProfilePage() {
           })
       }) 
   }
-  const onDeleteAccount = (e) => {
-    console.log("delete account");
+  const onDeleteAccount = async (e) => {
+    await fetch(`http://localhost:3000/api/user/${user._id}/delete`, {
+      method: "POST",
+      credentials: "include"
+    })
+    logoutUser(authDispatch, AUTH_ACTIONS);
   }
   return (
     <PageShell>
@@ -74,13 +91,35 @@ export default function ProfilePage() {
             </DropdownMenuTrigger>
             <DropdownMenuContent>
               <DropdownMenuItem>
-                <Icons.trash />
-                Delete Account
+                <AlertDialog>
+                  <AlertDialogTrigger 
+                    className="flex justify-center items-center space-x-2"
+                    onClick={(e) => {e.stopPropagation()}}
+                  >
+                    <Icons.trash />
+                    <span>Delete Account</span>
+                  </AlertDialogTrigger>
+                  <AlertDialogContent>
+                    <AlertDialogHeader>
+                      <AlertDialogTitle>Are you sure you want to delete your account?</AlertDialogTitle>
+                      <AlertDialogDescription>
+                        This will permanelty delete your accout. 
+                      </AlertDialogDescription>
+                    </AlertDialogHeader>
+                    <AlertDialogFooter>
+                      <AlertDialogCancel>Cancel</AlertDialogCancel>
+                      <AlertDialogAction onClick={onDeleteAccount}>Delete</AlertDialogAction>
+                    </AlertDialogFooter>
+                  </AlertDialogContent>
+                </AlertDialog>
               </DropdownMenuItem>
             </DropdownMenuContent>
+
           </DropdownMenu>
+
         </div>
       </section>
     </PageShell>
   )
 }
+
